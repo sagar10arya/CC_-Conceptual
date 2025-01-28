0137e5081b57bb316b500da7e45bdbd641691f37
sagar10arya <aryasagar125@gmail.com>
2025-01-28 12:29:17 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     // origin: process.env.CORS_ORIGIN || "http://localhost:5173", // Use environment variables for production
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true, // Allows cookies to be sent
   })
 );
