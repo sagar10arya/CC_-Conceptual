752962ca6c2609a885c066da436745494a88d1cd
sagar10arya <aryasagar125@gmail.com>
2025-02-05 20:40:29 +0530

    Reinstall dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+import "dotenv/config";
 import connectDB from "./src/db/index.js";
 import express from "express";
 import cors from "cors";
